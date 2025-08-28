d7b7e737335fb2735e0965c0c8ecd78289758340
Fha D. <43128683+ahfyks@users.noreply.github.com>
2025-08-28 17:14:21 +0700

    Update SumData.tsx
    Updated the calculation section due to the changes in the total full score.

--- components/SumData.tsx
+++ components/SumData.tsx
@@ -10,7 +10,7 @@ export const multiplyWeight = (data: number[]) => {
         let TempData = [...data];
         TempData.shift()
         let test = TempData.reduce((acc, i) =>  acc + Number(i) , 0)
-        output.push((test*(100/30)))
+        output.push((test*(100/29)))
         return output ;
     }
     
